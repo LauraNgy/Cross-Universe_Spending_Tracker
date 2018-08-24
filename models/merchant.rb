f3317dde19510f100b1e4b5c2393592fff338bbb
Laura Nagy <ngylaura@gmail.com>
2018-08-24 20:31:53 +0100

    adds save method to 'Merchant' class

--- models/merchant.rb
+++ models/merchant.rb
@@ -9,5 +9,19 @@ class Merchant
     @name = params['name']
   end
 
+  def save()
+    sql = "
+      INSERT INTO
+        merchants
+        (name)
+      VALUES
+        ($1)
+      RETURNING
+        id
+    "
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
 
 end
