34ccd0fd63a46341d81943f3102f0ba553ad1a48
Laura Nagy <ngylaura@gmail.com>
2018-08-24 16:21:26 +0100

    create 'accounts' table

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -14,11 +14,19 @@ CREATE TABLE merchants (
 );
 
 CREATE TABLE transactions (
-  id SERIAL4 PRIMARY KEY,
+  id SERIAL4,
   amount NUMERIC(12, 2),
   merchant_id INT4 REFERENCES merchants(id),
   tag_id INT4 REFERENCES tags(id),
   description VARCHAR(255),
-  currency VARCHAR(255),
+  currency VARCHAR(255) PRIMARY KEY,
   transaction_date DATE
+);
+
+CREATE TABLE accounts (
+  id SERIAL4 PRIMARY KEY,
+  holder VARCHAR(255),
+  currency_type VARCHAR(255) REFERENCES transactions(currency),
+  budget NUMERIC(12, 2),
+  balance NUMERIC(12, 2)
 );
