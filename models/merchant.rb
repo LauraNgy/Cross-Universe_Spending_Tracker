9a7200b646b413f690c6e30f94c144837c0408ef
Laura Nagy <ngylaura@gmail.com>
2018-08-24 20:59:25 +0100

    finishes CRUD for 'Merchant' class

--- models/merchant.rb
+++ models/merchant.rb
@@ -47,8 +47,30 @@ class Merchant
     return  result
   end
 
+  def self.find(id)
+    sql = "
+      SELECT * FROM
+        merchants
+      WHERE
+        id = $1
+    "
+    values = [id]
+    merchant = SqlRunner.run(sql, values)[0]
+    return Merchant.new(merchant)
+  end
+
   def self.map_items(merchant_data)
     return merchant_data.map{ |merchant| Merchant.new(merchant)}
+  end
+
+  def self.delete(id)
+    sql = "
+      DELETE FROM merchants
+      WHERE
+        id = $1
+    "
+    values = [id]
+    SqlRunner.run(sql, values)
   end
 
   def self.delete_all()
