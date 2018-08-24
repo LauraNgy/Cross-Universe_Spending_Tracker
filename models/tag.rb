34f46c3cda62421eb8d901e8bc7ff99089c604d4
Laura Nagy <ngylaura@gmail.com>
2018-08-24 17:59:52 +0100

    adds save method to 'Tag' class

--- models/tag.rb
+++ models/tag.rb
@@ -9,7 +9,18 @@ class Tag
     @name = params['name']
   end
 
-
+  def save()
+    sql = "
+      INSERT INTO tags
+      (name)
+      VALUES
+      ($1)
+      RETURNING id
+    "
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
 
   def self.delete_all()
     sql = "
