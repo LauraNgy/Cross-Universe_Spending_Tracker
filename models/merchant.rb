9ee6a837ec235e5bdfac6c490827916044623a3a
Laura Nagy <ngylaura@gmail.com>
2018-08-24 20:19:34 +0100

    initializes 'Merchant' class

--- models/merchant.rb
+++ models/merchant.rb
@@ -2,7 +2,12 @@ require_relative('../db/sql_runner')
 
 class Merchant
 
+  attr_accessor :id, :name
 
+  def initialize(params)
+    @id = params['id'].to_i if params['id']
+    @name = params['name']
+  end
 
 
 end
