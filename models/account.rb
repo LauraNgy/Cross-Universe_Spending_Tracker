fde0b3eae2996e3e1282c964786791f221239c3b
Laura Nagy <ngylaura@gmail.com>
2018-08-26 18:11:05 +0100

    initializes 'Account' class

--- models/account.rb
+++ models/account.rb
@@ -1,3 +1,13 @@
 class Account
 
+  attr_accessor :id, :holder, :currency_type, :budget, :balance
+
+  def initialize(params)
+    @id = params['id'].to_i if params['id']
+    @holder = params['holder']
+    @currency_type = params['currency_type']
+    @budget = params['budget'].to_i
+    @balance = params['balance'].to_i
+  end
+
 end
