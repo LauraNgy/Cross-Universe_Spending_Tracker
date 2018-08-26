07c77d575407051d606fe79d93b1bf7a800e6b24
Laura Nagy <ngylaura@gmail.com>
2018-08-26 18:13:14 +0100

    rearranges table creation

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -1,5 +1,5 @@
-DROP TABLE accounts;
 DROP TABLE transactons;
+DROP TABLE accounts;
 DROP TABLE merchants;
 DROP TABLE tags;
 
@@ -13,6 +13,14 @@ CREATE TABLE merchants (
   name VARCHAR(255)
 );
 
+CREATE TABLE accounts (
+  id SERIAL4,
+  holder VARCHAR(255),
+  currency_type VARCHAR(255) PRIMARY KEY,
+  budget NUMERIC(12, 2),
+  balance NUMERIC(12, 2)
+);
+
 CREATE TABLE transactions (
   id SERIAL4 PRIMARY KEY,
   amount NUMERIC(12, 2),
@@ -21,12 +29,4 @@ CREATE TABLE transactions (
   description VARCHAR(255),
   currency VARCHAR(255) REFERENCES accounts(currency_type),
   transaction_date DATE
-);
-
-CREATE TABLE accounts (
-  id SERIAL4,
-  holder VARCHAR(255),
-  currency_type VARCHAR(255) PRIMARY KEY,
-  budget NUMERIC(12, 2),
-  balance NUMERIC(12, 2)
 );
