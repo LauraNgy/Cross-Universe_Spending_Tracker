868bcd90844e2c27a4be4b4ca36991491b1263b3
Laura Nagy <ngylaura@gmail.com>
2018-08-26 18:07:00 +0100

    updates reference to accounts table'

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -14,19 +14,19 @@ CREATE TABLE merchants (
 );
 
 CREATE TABLE transactions (
-  id SERIAL4,
+  id SERIAL4 PRIMARY KEY,
   amount NUMERIC(12, 2),
   merchant_id INT4 REFERENCES merchants(id),
   tag_id INT4 REFERENCES tags(id),
   description VARCHAR(255),
-  currency VARCHAR(255) PRIMARY KEY,
+  currency VARCHAR(255) REFERENCES accounts(currency_type),
   transaction_date DATE
 );
 
 CREATE TABLE accounts (
-  id SERIAL4 PRIMARY KEY,
+  id SERIAL4,
   holder VARCHAR(255),
-  currency_type VARCHAR(255) REFERENCES transactions(currency),
+  currency_type VARCHAR(255) PRIMARY KEY,
   budget NUMERIC(12, 2),
   balance NUMERIC(12, 2)
 );
