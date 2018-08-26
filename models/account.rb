2b8b94dbfe414164cfa9d847a02a431f2947e591
Laura Nagy <ngylaura@gmail.com>
2018-08-26 19:42:01 +0100

    adds save method to 'Acccount' class

--- models/account.rb
+++ models/account.rb
@@ -1,3 +1,5 @@
+require_relative('../db/sql_runner')
+
 class Account
 
   attr_accessor :id, :holder, :currency_type, :budget, :balance
@@ -8,6 +10,19 @@ class Account
     @currency_type = params['currency_type']
     @budget = params['budget'].to_i
     @balance = params['balance'].to_i
+  end
+
+  def save()
+    sql = "
+      INSERT INTO accounts
+        (holder, currency_type, budget, balance)
+      VALUES
+        ($1, $2, $3, $4)
+      RETURNING id
+    "
+    values = [@holder, @currency_type, @budget, @balance]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
   end
 
 end
