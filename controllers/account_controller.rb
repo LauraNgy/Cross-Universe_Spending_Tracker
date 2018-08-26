b9f2cc55cd58588977d99e7f65effebfe87bbf79
Laura Nagy <ngylaura@gmail.com>
2018-08-26 23:46:34 +0100

    sets up main page of app

---
+++ controllers/account_controller.rb
@@ -0,0 +1,6 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/account.rb')
+require_relative('../models/transaction.rb')
+require('pry-byebug')
+also_reload('../models/*')
