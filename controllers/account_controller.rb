7776d088ccade0fa5275ce11d1381a5b2c0c9b74
Laura Nagy <ngylaura@gmail.com>
2018-08-27 09:56:07 +0100

    adds display for accounts

--- controllers/account_controller.rb
+++ controllers/account_controller.rb
@@ -4,3 +4,8 @@ require_relative('../models/account.rb')
 require_relative('../models/transaction.rb')
 require('pry-byebug')
 also_reload('../models/*')
+
+get '/accounts' do
+  @accounts = Account.all()
+  erb (:"accounts/index")
+end
