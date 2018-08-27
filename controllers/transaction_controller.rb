3db50f7be8295bfe4fec02b197b6d27b0b0a4082
Laura Nagy <ngylaura@gmail.com>
2018-08-27 14:12:16 +0100

    sets up transaction_controller

---
+++ controllers/transaction_controller.rb
@@ -0,0 +1,22 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/account.rb')
+require_relative('../models/transaction.rb')
+require_relative('../models/merchant.rb')
+require_relative('../models/tag.rb')
+require('pry-byebug')
+also_reload('../models/*')
+
+# index
+
+# new
+
+# create
+
+# show
+
+# edit
+
+# update
+
+#delete
