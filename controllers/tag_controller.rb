b4e0aab5a8187f4c27d197b48522886055aa08c6
Laura Nagy <ngylaura@gmail.com>
2018-08-28 16:03:14 +0100

    finishes RESTful routes for tags

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -6,21 +6,45 @@ require('pry-byebug')
 also_reload('../models/*')
 
 # index
-
+get '/tags' do
+  @tags = Tag.all()
+  erb (:"tags/index")
+end
 
 # new
-
+get '/tags/new' do
+  erb (:"tags/new")
+end
 
 # create
-
+post '/tags' do
+  @new_tag = Tag.new(params)
+  @new_tag.save()
+  redirect to '/done'
+end
 
 # show
-
+get '/tags/:id' do
+  @tag = Tag.find(params['id'].to_i)
+  erb (:"tags/show")
+end
 
 # edit
-
+get '/tags/:id/edit' do
+  @tag = Tag.find(params['id'].to_i)
+  erb (:"tags/edit")
+end
 
 # update
-
+post '/tags/:id' do
+  @tag = Tag.new(params)
+  @tag.update()
+  redirect to '/done'
+end
 
 # delete
+post '/tags/:id/delete' do
+  @tag = Tag.find(params['id'].to_i)
+  Transaction.delete_tag(@tag)
+  redirect to '/done'
+end
