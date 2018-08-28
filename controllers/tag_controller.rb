aaece3d9b65abd1fff8747fd47d159493b68edf2
Laura Nagy <ngylaura@gmail.com>
2018-08-28 15:32:22 +0100

    sets up tags_controller

---
+++ controllers/tag_controller.rb
@@ -0,0 +1,26 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/tag.rb')
+require_relative('../models/transaction.rb')
+require('pry-byebug')
+also_reload('../models/*')
+
+# index
+
+
+# new
+
+
+# create
+
+
+# show
+
+
+# edit
+
+
+# update
+
+
+# delete
